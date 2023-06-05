add23654a76c0b45e63a97fd5972e298a0571f14
Yuan Wang <ywang0990@gmail.com>
2023-06-04 22:32:06 -0700

    calibration results

--- analyze_results.py
+++ analyze_results.py
@@ -2,6 +2,25 @@ import pandas as pd
 import statistics
 
 # babbage
+# df = pd.read_csv('predicted_results_eec_babbage_with_prob.csv')
+
+# emotion_word_avg = df.groupby(['emotion', 'emotion_word']).agg(
+#     avg_intensity=pd.NamedAgg(column='predicted_intensity', aggfunc=statistics.mean)
+# )
+# emotion_word_avg.to_csv('analysis/babbage/emotion_word_avg_table.csv')
+
+# emotion_word_avg_by_gender = df.groupby(['emotion', 'gender']).agg(
+#     avg_intensity=pd.NamedAgg(column='predicted_intensity', aggfunc=statistics.mean)
+# )
+# emotion_word_avg_by_gender.to_csv('analysis/babbage/emotion_word_avg_gender_table.csv')
+
+# emotion_word_avg_by_race = df.groupby(['emotion', 'race']).agg(
+#     avg_intensity=pd.NamedAgg(column='predicted_intensity', aggfunc=statistics.mean)
+# )
+# emotion_word_avg_by_race.to_csv('analysis/babbage/emotion_word_avg_race_table.csv')
+
+# _OLD
+# babbage
 # df = pd.read_csv('predicted_results_eec_babbage.csv')
 
 # emotion_word_avg = df.groupby(['emotion', 'emotion_word']).agg(
