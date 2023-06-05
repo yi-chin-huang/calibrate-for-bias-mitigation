ad0060971fb9f7bb2f138f47a2ca9458116cb6a9
Yuan Wang <ywang0990@gmail.com>
2023-06-04 22:54:10 -0700

    calibrated analysis

--- analyze_results.py
+++ analyze_results.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import statistics
 
+# babbage:calibrated
+df = pd.read_csv('calibrate/calibrated_results.csv')
+
+emotion_word_avg = df.groupby(['emotion', 'emotion_word']).agg(
+    avg_intensity=pd.NamedAgg(column='calibrated_intensity', aggfunc=statistics.mean)
+)
+emotion_word_avg.to_csv('analysis/calibrated/emotion_word_avg_table.csv')
+
+emotion_word_avg_by_gender = df.groupby(['emotion', 'gender']).agg(
+    avg_intensity=pd.NamedAgg(column='calibrated_intensity', aggfunc=statistics.mean)
+)
+emotion_word_avg_by_gender.to_csv('analysis/calibrated/emotion_word_avg_gender_table.csv')
+
+emotion_word_avg_by_race = df.groupby(['emotion', 'race']).agg(
+    avg_intensity=pd.NamedAgg(column='calibrated_intensity', aggfunc=statistics.mean)
+)
+emotion_word_avg_by_race.to_csv('analysis/calibrated/emotion_word_avg_race_table.csv')
+
 # babbage
 # df = pd.read_csv('predicted_results_eec_babbage_with_prob.csv')
 
